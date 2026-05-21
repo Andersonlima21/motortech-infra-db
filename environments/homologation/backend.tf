2f3e7ca4632316c2b96547d2edbb7fa141a7fc72
Anderson lima <andersonlimadev21@gmail.com>
2026-05-20 22:39:09 -0300

    Fix

--- environments/homologation/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "motortech-tf-state"
-    key            = "infra-db/homologation/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "motortech-tf-lock"
-    encrypt        = true
-  }
-}
--- environments/production/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "motortech-tf-state"
-    key            = "infra-db/production/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "motortech-tf-lock"
-    encrypt        = true
-  }
-}
